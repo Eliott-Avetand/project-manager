acca399892c5db14e3e02c9b9e2184977be3caed
Eliott-Avetand <eliott.avetand@epitech.eu>
2023-06-16 11:27:34 +0200

    [~] Changed the cors configuration for the server

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { readFileSync } from 'fs';
+import express from 'express';
 
 async function bootstrap() {
     const httpsOptions = {
@@ -11,8 +12,16 @@ async function bootstrap() {
     };
     const app = await NestFactory.create<NestExpressApplication>(AppModule, { httpsOptions });
 
-    app.enableCors({ origin: 'http://localhost:3000', credentials: true });
+    app.enableCors({
+        origin: ['http://localhost:3000', 'https://loustik-manager.fr'],
+        credentials: true,
+        methods: "GET, POST, OPTIONS, PUT, DELETE",
+        optionsSuccessStatus: 200,
+        allowedHeaders: "*"
+    });
     app.use(cookieParser());
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
 
     await app.listen(8080);
 }
